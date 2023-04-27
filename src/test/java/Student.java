a09f40a069f07aa6e95dc9dde1fd8a8a8fdfb6e2
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-04-27 16:32:20 -0500

    junitexercise

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,32 +1,22 @@
+import java.util.ArrayList;
+
 public class Student {
+public long getId (){
+    return 0;
+}
+public String getName (){
+    return name;
+}
+public void addGrade(int grade) {
+}
+public ArrayList<integer> getGrades (){
+    return grades;
+}
+public double getGradeAverage (){
+    return average;
+}
+public Student() {
+}
 
-    public String getName (){
-        return name;
-    }
-    public void addGrade(int grade){
-        grades.add(grade);
-    }
-    public ArrayList<Integer> getGrades(){
-        return grades;
-    }
-    public double getGradeAverage(){
-        int sum = 0;
-        for (int grade : grades){
-            sum += grade;
-        }
-        return sum / grades.size();
-    }
-    public Student(String getName) {
-        this.getName = getName;
-    }
-    public String getGetName() {
-        return getName;
-    }
-    public void setGetName(String getName) {
-        this.getName = getName;
-    }
-
-    public Student() {
-    }
 
 }
